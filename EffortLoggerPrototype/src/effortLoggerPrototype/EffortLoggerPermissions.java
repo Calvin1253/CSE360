d48260b24a6fd51a66d5748bbb47d5019a9785a0
Calvin1253 <110355250+Calvin1253@users.noreply.github.com>
2023-10-26 23:07:59 -0700

    Add files via upload
    This is my project file just so you guys have it I couldn't figure this out completely lol

---
+++ EffortLoggerPrototype/src/effortLoggerPrototype/EffortLoggerPermissions.java
@@ -0,0 +1,12 @@
+package effortLoggerPrototype;
+
+public class EffortLoggerPermissions {
+
+    public static boolean isIdValid(String id) {
+        // Add your ID validation logic here
+        // Return true if the ID is valid, otherwise return false
+        return id != null && !id.isEmpty();
+    }
+	
+	
+}
---
+++ EffortLoggerPrototype/src/effortLoggerPrototype/EffortLoggerPrototype.java
@@ -0,0 +1,105 @@
+package effortLoggerPrototype;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class EffortLoggerPrototype extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) {
+        System.out.println("Effort Logger Console Started!");
+        primaryStage.setTitle("Effort Logger Prototype");
+
+        // Create text fields for ID and password with black text and white background
+        TextField idTextField = new TextField();
+        idTextField.setPromptText("ID");
+        idTextField.setStyle("-fx-text-fill: white; -fx-control-inner-background: black; -fx-border-color: white;");
+
+        TextField passwordTextField = new TextField();
+        passwordTextField.setPromptText("Password");
+        passwordTextField.setStyle("-fx-text-fill: white; -fx-control-inner-background: black; -fx-border-color: white;");
+
+        // Create labels for text fields with white text
+        Label idLabel = new Label("ID:");
+        Label passwordLabel = new Label("Password:");
+        idLabel.setTextFill(Color.WHITE);
+        passwordLabel.setTextFill(Color.WHITE);
+
+        Button loginButton = new Button("Login");
+        loginButton.setMinWidth(Button.USE_PREF_SIZE);
+        loginButton.setStyle("-fx-text-fill: white; -fx-background-color: black; -fx-border-color: white;");
+        loginButton.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                String id = idTextField.getText();
+                String password = passwordTextField.getText();
+                System.out.println("ID: " + id + ", Password: " + password);
+            }
+        });
+
+        // Create a grid pane to organize the elements with padding
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(10));
+        grid.add(idLabel, 0, 0);
+        grid.add(idTextField, 1, 0);
+        grid.add(passwordLabel, 0, 1);
+        grid.add(passwordTextField, 1, 1);
+        grid.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY))); // Set the background to black
+
+        
+        // Create a layout for centering
+        BorderPane root = new BorderPane();
+        root.setCenter(grid);
+        root.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+        
+        // Create a label for the title with custom font and white text
+        Label titleLabel = new Label("Effort Logger");
+        titleLabel.setFont(new Font("Lucida Console", 20)); // Set the font to Lucida Console with size 20
+        titleLabel.setTextFill(Color.WHITE);
+        titleLabel.setPadding(new Insets(20));
+        
+        // Create a layout for centering
+        root.setTop(titleLabel); // Add the title label at the top
+        BorderPane.setAlignment(titleLabel, Pos.CENTER);
+        
+        // Place the "Login" button in the bottom right corner
+        HBox buttonBox = new HBox(10);
+        buttonBox.setPadding(new Insets(10));
+        buttonBox.getChildren().add(loginButton);
+        buttonBox.setAlignment(Pos.BOTTOM_RIGHT);
+        root.setBottom(buttonBox);
+
+        // Set constraints to make text fields take up 50% of the window
+        idTextField.setMaxWidth(Double.MAX_VALUE);
+        passwordTextField.setMaxWidth(Double.MAX_VALUE);
+
+        // Set the stage to not be resizable
+        primaryStage.setResizable(false);
+
+        // Set the background color for the entire scene to black
+        Scene scene = new Scene(root, 300, 250);
+        scene.setFill(Color.BLACK);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
