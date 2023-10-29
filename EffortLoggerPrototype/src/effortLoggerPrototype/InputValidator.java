e6670a41036aa00b2f60995e3293295128142fb2
Abhijit Girish <61362745+HiroyukiAbhi@users.noreply.github.com>
2023-10-28 21:26:52 -0700

    Update InputValidator.java
    Updated Number recognition

--- EffortLoggerPrototype/src/effortLoggerPrototype/InputValidator.java
+++ EffortLoggerPrototype/src/effortLoggerPrototype/InputValidator.java
@@ -5,21 +5,29 @@ public class InputValidator {
 
 	public Object input;
 	public int returnVal; // 0 - NormalString, 1 - String with Special Characters, 2 - Integer, 3 - Double, 4 - INVALID(Unkown)
+	// 5 - Number string
 	HashMap<Character, Integer> alphabet = new HashMap<>();
 	HashMap<Character, Integer> SpecialChar = new HashMap<>();
-
+	HashMap<Character, Integer> Nums = new HashMap<>();
 	public InputValidator(Object input) {
 		this.input = input;
 		returnVal = 20;
 		int i = 0;
-		// Create a HashMap for the alphabet containing lowercase letters (a-z)
+		// Create a Hashmap for the alphabet containing lowercase letters (a-z)
 		for (char c = 'a'; c <= 'z'; c++) {
 			this.alphabet.put(c, i);
 			i++;
 		}
-		// Add uppercase letters (A-Z) to the alphabet HashMap
+		// Add uppercase letters (A-Z) to the alphabet Hashmap
+		i = 0;// Reset i
 		for (char c = 'A'; c <= 'Z'; c++) {
-			alphabet.put(c, 1);
+			alphabet.put(c, i);
+			i++;
+		}
+		i = 0;
+		for (char x = '0'; x <= '9'; x++) {
+			Nums.put(x, i);
+			i++;
 		}
 
 		//For special Characters
@@ -37,12 +45,16 @@ public class InputValidator {
 	public int checkType(Object input) {
 		if (input instanceof String) {
 			// Check if the string contains only normal alphabet characters
-			if (charactersExistInAlphabet()) {
+			if (charactersExistInAlphabet((String)input)) {
 				this.returnVal = 0; // String with only normal Strings
 			}
 			// Check if the string contains both normal and special characters
-			else if (charactersExistInAlphabetAndSpecial()) {
+			else if (charactersExistInAlphabetAndSpecial((String)input)) {
 				this.returnVal = 1; // SpecialChar String
+			}
+			else if (charIntegers((String)input)) {
+				
+				this.returnVal = 5; // Number String
 			}
 		} else if (input instanceof Integer) {
 			this.returnVal = 2;
@@ -51,12 +63,13 @@ public class InputValidator {
 		} else {
 			this.returnVal = 4; // INVALID (Unknown)
 		}
+		System.out.print(returnVal);
 		return returnVal;
 	}
 
 	// Check if all characters in the input string are in the alphabet
-	public boolean charactersExistInAlphabet() {
-		String inputString = (String) this.input;
+	public boolean charactersExistInAlphabet(String input) {
+		String inputString = (String) input;
 		char[] inputChars = inputString.toCharArray();
 		for (char c : inputChars) {
 			if (!this.alphabet.containsKey(c)) {
@@ -67,11 +80,21 @@ public class InputValidator {
 	}
 
 	// Check if all characters in the input string are in either the alphabet or special characters
-	public boolean charactersExistInAlphabetAndSpecial() {
-		String inputString = (String) this.input;
+	public boolean charactersExistInAlphabetAndSpecial(String input) {
+		String inputString = (String) input;
 		char[] inputChars = inputString.toCharArray();
 		for (char c : inputChars) {
 			if (!this.alphabet.containsKey(c) && !this.SpecialChar.containsKey(c)) {
+				return false; // Character not in the alphabet or special characters
+			}
+		}
+		return true; // All characters are in the alphabet or special characters
+	}
+	public boolean charIntegers(String input) {
+		String inputString = (String) input;
+		char[] inputChars = inputString.toCharArray();
+		for (char c : inputChars) {
+			if (!this.Nums.containsKey(c)) {
 				return false; // Character not in the alphabet or special characters
 			}
 		}
